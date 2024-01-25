d3313ca9d7d81c46f08f828755b50a86a8de7fa7
Nursultan Nurtay <82012495+Idontusethisanyways@users.noreply.github.com>
2024-01-25 22:45:08 +0600

    Update COVID DATA SQL DATA EXPLORATION PROJECT.sql

--- COVID DATA SQL DATA EXPLORATION PROJECT.sql
+++ COVID DATA SQL DATA EXPLORATION PROJECT.sql
@@ -7,42 +7,44 @@ order by 3, 4
 --From PortfolioProject..CovidVaccinations
 --order by 3, 4
 
-
+--Selecting data that we are going to be using
 Select Location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProject..CovidDeaths
 order by 1,2
 
-
+--Total case vs total deaths
 Select Location, date, total_cases,total_deaths, (cast(total_deaths as float)/cast(total_cases as float)) * 100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
 where location like '%Kazakhstan%'
 order by 1,2
 
+--Total cases vs population
 Select Location, date, total_cases,population, (cast(total_cases as float)/population) * 100 as DeathPercentage
 From PortfolioProject..CovidDeaths
 where location like '%Kazakhstan%'
 order by 1,2
 
-
+--Adding Highest Infection count out of total population by countries
 Select Location, Population, MAX(cast(total_cases as float)) as HighestInfectionCount, MAX((cast(total_cases as float)/population)) * 100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
 Group by Location, Population
 order by PercentPopulationInfected desc
 
+--Total death count by location
 Select Location, MAX(cast(total_deaths as float)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
 Where continent is not null
 Group by Location
 order by TotalDeathCount desc
 
-
+--Total death count by continent (accurate)
 Select location, MAX(cast(total_deaths as float)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
 Where continent is null and location not like '%income%'
 Group by Location
 order by TotalDeathCount desc
 
-
+--Total death count by continent (inaccurate data is incorect)
 Select continent, MAX(cast(total_deaths as float)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
 Where continent is not null
